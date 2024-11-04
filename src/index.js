6b6e30dd165342762b218334a8965bbbaf8a578d
Bastiancito <bastian.camusj@usm.cl>
2024-11-04 01:44:03 -0300

    algunos cambios

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 
 import Layout from './components/layout'
-import Postulaciones from './components/postulaciones'
 
 import './stylesheets/index.scss'
 
 document.body.innerHTML = '<div id="root"></div>';
 const root = createRoot(document.getElementById('root'));
-root.render(<Postulaciones />);
+root.render(<Layout />);
--- src/pages/postulaciones.jsx
+++ src/pages/postulaciones.jsx
@@ -1,15 +1,68 @@
 import React from 'react';
 import LogoBanner from '../components/logo_banner';
 import Footer from '../components/footer';
+import '../stylesheets/postulaciones.scss';
 
 export const Postulaciones = () => {
   return (
     <>
-    <LogoBanner className='logo-banner'/>
-      <p>¡Postulaciones!</p>
-      <Footer className='footer'/>
-    </>
-  )
-}
+      <div className='postulaciones-container'>
+        <div className='left-container'>
+          <div className='secuencia'>
+            <div className='paso'>
+              <p>Datos Admision</p>
+            </div>
+            <div className='paso'>
+              <p>Datos Personales</p>
+            </div>
+            <div className='paso'>
+              <p>Datos Academicos</p>
+            </div>
+            <div className='paso'>
+              <p>Datos Adicionales</p>
+            </div>
+            <div className='paso'>
+              <p>Seleccion Carrera/Programa</p>
+            </div>
+            <div className='paso'>
+              <p>Documentos</p>
+            </div>
+          </div>
 
-export default Postulaciones
+          <div className='header'>
+            {/* Primer Grupo: Selecciona el Período */}
+            <div>
+              <label htmlFor='periodSelect'>Selecciona el período</label>
+              <select id='periodSelect' className='select'>
+                <option value="Primer Periodo 2025">Primer Periodo 2025</option>
+              </select>
+            </div>
+
+            {/* Segundo Grupo: Selecciona tu Tipo de Postulación */}
+            <div>
+              <label htmlFor='admissionSelect'>Selecciona tu tipo de postulación</label>
+              <select id='admissionSelect' className='select'>
+                <option value="Admision especial">Admisión Especial</option>
+              </select>
+            </div>
+            <button className='button'>Siguiente</button>
+          </div>
+        </div>
+
+        {/* Contenedor para los nuevos divs a la derecha */}
+        <div className='right-container'>
+          <div className='right-div'>
+            <p>Contenido del Div 1</p> {/* Contenido del primer div */}
+          </div>
+          <div className='right-div'>
+            <p>Contenido del Div 2</p> {/* Contenido del segundo div */}
+          </div>
+        </div>
+      </div>
+
+      <Footer className='footer' />
+    </>
+  );
+};
+
+export default Postulaciones;
