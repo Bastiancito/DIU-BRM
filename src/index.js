cfd851986934e50f79e1195b8e3c80a54982fa69
Bastiancito <bastian.camusj@usm.cl>
2024-11-03 22:05:25 -0300

    alo

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 
 import Layout from './components/layout'
+import Postulaciones from './components/postulaciones'
 
 import './stylesheets/index.scss'
 
 document.body.innerHTML = '<div id="root"></div>';
 const root = createRoot(document.getElementById('root'));
-root.render(<Layout />);
+root.render(<Postulaciones />);
